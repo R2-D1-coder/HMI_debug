0ead12bfb18293e6aad6599c3df72723c0360d5f
yang donghao <yangdonghao@dreame.tech>
2020-07-02 21:42:09 +0800

    实时检测文件数据并输出变更字段

--- renderer.js
+++ renderer.js
@@ -1,17 +1,33 @@
 var fs = require("fs");
 
 var filePath =
-  "D:/debug/software/cleanbot_visualizer_release_mingw64/online_log/";
+  "D:/debug/software/cleanbot_visualizer_release_mingw64/online_log/online_P7Log_String_20200702-213443.txt";
 
-fs.watch(filePath, function (event, filename) {
-      console.log('event is: ' + event);
-      if (filename) {
-         console.log('filename provided: ' + filename);
-        //readTxt();
+fs.open(filePath, "a+", function (error, fd) {
+  var buffer;
+  var remainder = null;
+  fs.watchFile(
+    filePath,
+    {
+      persistent: true,
+      interval: 100,
+    },
+    function (curr, prev) {
+      //console.log(curr);
+      if (curr.mtime > prev.mtime) {
+        //文件内容有变化，那么通知相应的进程可以执行相关操作。例如读物文件写入数据库等
+        buffer = new Buffer(curr.size - prev.size); // 创建一个缓冲，长度为（当前文件大小-文件上一个状态的大小）
+        fs.read(fd, buffer, 0, curr.size - prev.size, prev.size, function (
+          err,
+          bytesRead,
+          buffer
+        ) {
+          //console.log(buffer.toString()); //新增加的内容
+          console.log(curr.mtime);
+        });
       } else {
-        console.log('filename not provided');
+        console.log("文件读取错误");
       }
-  }
-);
-console.log(filePath + ' 被监听中...');
-
+    }
+  );
+});
